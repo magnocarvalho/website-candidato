23c24f18d7eba5a3bfb19865316f0cbb1c8f3c93
magnocarv <magnocarv@hotmail.com>
2020-05-22 02:26:21 -0300

    cofiguraçoes do deploy para o firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB7XZX-errVVFhvQrT_0gW527n-i9L1KE4",
+    authDomain: "poletize-beta.firebaseapp.com",
+    databaseURL: "https://poletize-beta.firebaseio.com",
+    projectId: "poletize-beta",
+    storageBucket: "poletize-beta.appspot.com",
+    messagingSenderId: "192944379066",
+    appId: "1:192944379066:web:c0474998d809d659cb5e83",
+  },
 };
