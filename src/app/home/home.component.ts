616c6b62d9c392e3ec5931a13946af70350641dc
Magno Carvalho <magnocarv@hotmail.com>
2020-06-09 05:06:41 -0300

    home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,45 +9,7 @@ import { LoadingBarService } from "@ngx-loading-bar/core";
 	styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-	profileForm: FormGroup;
-	isValid = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/;
-	constructor(private api: ApiService, private loadingBar: LoadingBarService) {
-		this.profileForm = new FormGroup({
-			nome: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-			email: new FormControl("", [
-				Validators.required,
-				Validators.minLength(3),
-				Validators.maxLength(50),
-				Validators.email,
-			]),
-			telefone: new FormControl("", [
-				Validators.required,
-				Validators.minLength(10),
-				Validators.maxLength(11),
-				Validators.pattern(this.isValid),
-			]),
-			cidade: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-			descricao: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(250)]),
-			escritorio: new FormControl(10),
-			tipo: new FormControl(5),
-			dataReivindicacao: new FormControl(new Date()),
-		});
-	}
+	constructor(private api: ApiService, private loadingBar: LoadingBarService) {}
 
 	ngOnInit(): void {}
-
-	enviarContato($event) {
-		if (this.profileForm.valid) {
-			console.log(this.profileForm.value);
-			this.loadingBar.start();
-			this.api
-				.createUsuario(this.profileForm.value)
-				.then((res) => {
-					this.profileForm.disable();
-				})
-				.finally(() => this.loadingBar.complete());
-		} else {
-			this.profileForm.markAllAsTouched();
-		}
-	}
 }
